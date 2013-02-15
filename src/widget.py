318daa92bffa30168eb7409dfc0a16e27fde7add
Til Birnstiel <birnstiel@me.com>
2013-02-15 14:12:58 -0500

    replaced old widget with completely rewritten version

--- src/widget.py
+++ src/widget.py
@@ -1,268 +1,269 @@
+#!/usr/bin/env python
 import numpy as np
-import glob
 import matplotlib.pyplot as plt
-#from matplotlib import colorbar
-from matplotlib.widgets import Button
+from matplotlib.widgets import Slider, Button
+import sys
+import os
 
-class plotter:
-    #
-    # the initialization
-    #
-    def __init__(self,x=[],y=[],data=[],times=[],i_start=0,xlog=0,ylog=0,zlog=0,xlim=[],ylim=[],zlim=[],
-        xlabel='',ylabel='',lstyle='-',
-        data2=[],transparent=True):
-        self.transparent=transparent
-        self.fig=plt.figure()
-        #
-        # some fiducial data if no data is given 
-        #
-        if (x==[])&(data==[]):
-            self.times = np.arange(2, 20, 1)
-            self.freq  = np.arange(2, 20, 1)
-            self.x     = np.arange(0.0, 1.0, 0.001)
-            self.data  = np.zeros([len(self.times),len(self.x)])
-            for j in np.arange(0,len(self.times),1):
-                self.data[j] = np.sin(2*np.pi*self.freq[j]*self.x)
-            self.data2 = []
-            self.oneD  = 1
-        else:
-            self.x     = x
-            self.data  = data
-            self.data2 = data2
-            self.xlim  = xlim
-            self.ylim  = ylim
-            self.zlim  = zlim
-            #
-            # distinguish between 1D and 2D version
-            #
-            self.y = y
-            if not(y==[]):
-                self.oneD = 0
-                self.n_t  = self.data.shape[0]/len(y)
-            else:
-                self.oneD = 1
-                self.n_t  = self.data.shape[0]
-        #
-        # if no time is given, make an index
-        #
-        if (times==[]):
-            self.times = np.arange(0, self.data.shape[0], 1)
-        else:
-            self.times = times
-        #
-        # save the number of grid points
-        #   
-        self.n_x   = len(self.x)
-        self.n_y   = len(self.y)
-        #
-        # wether or not x or y are logarithmic
-        #
-        self.lstyle    = lstyle
-        self.xlog      = xlog
-        self.ylog      = ylog
-        self.zlog      = zlog
-        self.show_time = 1
-        self.xlabel    = xlabel
-        self.ylabel    = ylabel
-        self.ind       = i_start
-        #
-        # the setup
-        #
-        self.ax = plt.subplot(111)
-        plt.subplots_adjust(bottom=0.2)
-        #
-        # buttons to go +1 or -1
-        #
-        self.ax_f = plt.axes([0.50, 0.05, 0.05, 0.05])
-        self.ax_r = plt.axes([0.45, 0.05, 0.05, 0.05])
-        self.b_f = Button(self.ax_f, '>')
-        self.b_f.on_clicked(self.f)
-        self.b_r = Button(self.ax_r, '<')
-        self.b_r.on_clicked(self.r)
-        #
-        # buttons to go +10 or -10
-        #
-        self.ax_ff = plt.axes([0.55, 0.05, 0.05, 0.05])
-        self.ax_rr = plt.axes([0.40, 0.05, 0.05, 0.05])
-        self.b_ff = Button(self.ax_ff, '>>')
-        self.b_ff.on_clicked(self.ff)
-        self.b_rr = Button(self.ax_rr, '<<')
-        self.b_rr.on_clicked(self.rr)
-        #
-        # buttons to go to start or end
-        #
-        self.ax_fff = plt.axes([0.60, 0.05, 0.05, 0.05])
-        self.ax_rrr = plt.axes([0.35, 0.05, 0.05, 0.05])
-        self.b_fff = Button(self.ax_fff, '|>')
-        self.b_fff.on_clicked(self.fff)
-        self.b_rrr = Button(self.ax_rrr, '<|')
-        self.b_rrr.on_clicked(self.rrr)
-        #
-        # the log buttons
-        #
-        self.ax_xlog = plt.axes([0.70, 0.05, 0.05, 0.05])
-        self.ax_ylog = plt.axes([0.75, 0.05, 0.05, 0.05])
-        self.b_xlog = Button(self.ax_xlog, 'xlog')
-        self.b_xlog.on_clicked(self.setxlog)
-        self.b_ylog = Button(self.ax_ylog, 'ylog')
-        self.b_ylog.on_clicked(self.setylog)
-        #
-        # the print button
-        #
-        self.ax_print = plt.axes([0.85, 0.05, 0.075, 0.05])
-        self.b_print  = Button(self.ax_print, 'print')
-        self.b_print.on_clicked(self.printfig)
-        #
-        # call the plotcommand to
-        # print the initial frame
-        #
-        self.plotcommand(0)
-        plt.show()
-    #
-    # the forward and backward commands
-    #
-    def f(self, event):
-        self.ind += 1
-        if (self.ind > self.n_t-1):
-            self.ind = self.n_t-1            
-        self.plotcommand(self.ind)
-        
-    def ff(self, event):
-        self.ind += 10
-        if (self.ind > self.n_t-1):
-            self.ind = self.n_t-1            
-        self.plotcommand(self.ind)
-        
-    def fff(self, event):
-        self.ind = self.n_t-1
-        self.plotcommand(self.ind)
+def main():
+	x = np.linspace(1.0, 10.0, 200)
+	y = np.linspace(1.0, 10.0, 200)
+	
+	times = np.linspace(1.0,11.0,100)
+	data1D = np.array([t*np.sin(x*np.pi*3)+6 for t in times])
+	data2D = np.zeros([len(y)*len(times),len(x)])
+	for it in np.arange(len(times)):
+		for iy in np.arange(len(y)):
+			data2D[it*len(y)+iy,:] = (x+y[iy])*np.exp(-6.0*((x-times[it])**2+(y[iy]-times[it])**2)) 
+	
+	plotter(x,data1D,times=times,data2=[0.5*data1D,0.25*data1D+5])
+	
+	plotter(x,data2D,y=y,times=times,data2=[data1D,0.5*data1D])
+	
+	plt.show()
 
-    def r(self, event):
-        self.ind -= 1
-        if (self.ind < 0):
-            self.ind = 0
-        self.plotcommand(self.ind)
-        
-    def rr(self, event):
-        self.ind -= 10
-        if (self.ind < 0):
-            self.ind = 0
-        self.plotcommand(self.ind)
-        
-    def rrr(self, event):
-        self.ind = 0
-        self.plotcommand(self.ind)
-        
-    def setxlog(self,event):
-        if self.xlog==1:
-            self.xlog = 0
-            self.ax.set_xscale('linear')
-            self.b_xlog.label.set_text('xlog')
-        else:
-            self.xlog = 1
-            self.ax.set_xscale('log')
-            self.b_xlog.label.set_text('xlin')
-        plt.draw()
-    
-    def setylog(self,event):
-        if self.ylog==1:
-            self.ylog = 0
-            self.ax.set_yscale('linear')
-            self.b_ylog.label.set_text('xlog')
-        else:
-            self.ylog = 1
-            self.ax.set_yscale('log')
-            self.b_ylog.label.set_text('xlin')
-        plt.draw()
-
-    def printfig(self,event):
-        #
-        # create figure & axes
-        #
-        F=plt.figure()
-        A=plt.axes()
-        if self.transparent==True:
-            A.set_axis_bgcolor('none')
-        #
-        # plot it
-        #
-        self.plotcommand(self.ind,AX=A)
-        #
-        # determine file name
-        #
-        i=len(glob.glob('figure_???.pdf'))+1
-        fname = 'figure_'+str(i).zfill(3)+'.pdf'
-        if self.transparent==True:
-            plt.savefig(fname, facecolor='none', edgecolor='none')
-        else:
-            plt.savefig(fname)        
-        print 'saved as '+fname
-        plt.close(F)
-    #
-    # this function does the actual drawing
-    #
-    def plotcommand(self,i,AX=None):
-        if AX==None:
-            AX=self.fig.get_axes()[0]
-        #
-        # if l does not exist yet (i.e. first call)
-        # then create it
-        #
-        AX.clear()
-        #
-        # if 1D plot
-        #
-        if (self.oneD==1):
-            AX.plot(self.x, self.data[i],self.lstyle,lw=2,markerfacecolor='none')
-            if not(self.data2==[]):
-                AX.plot(self.x, self.data2[i],self.lstyle,lw=2,markerfacecolor='none')
-        #
-        # if 2D plot
-        #
-        else:
-            if (self.zlog==1):
-                if (self.zlim==[]):
-                    Z    = np.log10(self.data[i*self.n_y+np.arange(0,self.n_y),:])
-                    AX.contourf(self.x,self.y,Z)
-                else:
-                    Z    = np.log10(self.zlim[0]+
-                        self.data[i*self.n_y+np.arange(0,self.n_y),:])
-                    AX.contourf(self.x,self.y,Z,
-                        np.arange(np.log10(self.zlim[0]),np.log10(self.zlim[1])))
-                    plt.hot()
-            else:
-                if (self.zlim==[]):
-                    Z    = self.data[i*self.n_y+np.arange(0,self.n_y),:]
-                    AX.contourf(self.x,self.y,Z)
-                else:
-                    Z    = self.data[i*self.n_y+np.arange(0,self.n_y),:]+self.zlim[0]
-                    AX.contourf(self.x,self.y,Z,np.arange(self.zlim[0],self.zlim[1]))
-        #
-        # log or lin axes
-        #
-        if (self.xlog==1):
-            AX.set_xscale('log')
-        if (self.ylog==1):
-            AX.set_yscale('log')
-        #
-        # limits
-        #
-        if not(self.xlim==[]):
-            AX.set_xlim(self.xlim)
-        if not(self.ylim==[]):
-            AX.set_ylim(self.ylim)
-        #
-        # title
-        #
-        if (self.show_time == 1):
-            AX.set_title(self.times[i])
-        #
-        # labels
-        #
-        AX.set_xlabel(self.xlabel)
-        AX.set_ylabel(self.ylabel)
-        #
-        # draw the plot
-        #
-        plt.draw()
-
+def plotter(x,data,y=None,data2=[],times=None,i_start=0,xlog=False,ylog=False,zlog=False,xlim=None,ylim=None,zlim=None,xlabel='',ylabel='',lstyle='-',ncont=None,cmap=None,fill=True):
+	#
+	# general setup
+	#
+	if y==None:
+		nt,nx = data.shape
+	else:
+		nx = data.shape[1]
+		ny = len(y)
+		nt = data.shape[0]/ny
+	#
+	# some size checks
+	#	
+	if nx!=len(x):
+		print('ERROR: len(x) does not match the number of columns of the data array')
+		sys.exit(1)
+	if times!=None:
+		if nt != len(times):
+			print('ERROR: len(times) does not match (number of rows)/ny of the data array')
+			sys.exit(1)
+	if y==None:
+		i_max  = data.shape[0]-1
+	else:
+		i_max  = nt-1
+	#
+	# color scheme 
+	#
+	if cmap==None: cmap='hot'
+	#
+	# convert data2 if necessary
+	#
+	if type(data2).__name__=='ndarray':
+		data2 = [data2]
+	#
+	# set limits
+	#
+	if xlim==None: xlim=[x[0],x[-1]]
+	if ylim==None:
+		if y==None:
+			ylim=[data.min(),data.max()]
+		else:
+			ylim=[y[0],y[-1]]
+	if zlim==None: zlim=[data.min(),data.max()]
+	#
+	# zlog cannot just be set, we need to convert the data
+	#
+	if zlog:
+		data=np.log10(data)
+		zlim=np.log10(zlim)
+	#
+	# add floor value
+	#
+	if fill:
+		data = np.maximum(data,zlim[0])
+	#
+	# set number of contours
+	#
+	if ncont==None:
+		ncont=zlim[-1]-zlim[0]+1
+	#
+	# set up figure
+	#
+	fig=plt.figure()
+	#
+	# ===============
+	# INITIAL DRAWING
+	# ===============
+	#
+	ax    = plt.subplot(111)
+	plt.subplots_adjust(left=0.25, bottom=0.25)
+	plt.axis([xlim[0], xlim[1], ylim[0], ylim[1]])
+	#
+	# draw labels
+	#
+	if xlabel!='': plt.xlabel(xlabel)
+	if ylabel!='': plt.ylabel(ylabel)
+	if times!=None: ti=plt.title('%g'%times[i_start])
+	#
+	# set scales
+	#
+	if xlog: ax.set_xscale('log')
+	if ylog: ax.set_yscale('log')
+	#
+	# plot the normal data
+	#
+	if y==None:
+		#
+		# line data
+		#
+		l, = ax.plot(x,data[i_start], lw=2,ls=lstyle)
+	else:
+		#
+		# 2D data
+		#
+		l  = ax.contourf(x,y,data[i_start*ny+np.arange(ny),:],np.linspace(zlim[0],zlim[-1],ncont),cmap=cmap)
+		clist = l.collections[:]
+	#
+	# plot additional line data
+	#
+	add_lines = []
+	for d in data2:
+		l2, = ax.plot(x,d[i_start], lw=2,ls=lstyle)
+		add_lines+=[l2]
+	#
+	# ========
+	# Make GUI
+	# ========
+	#
+	#
+	# make time slider
+	#
+	axcolor     = 'lightgoldenrodyellow'
+	ax_time     = plt.axes([0.25, 0.1, 0.65, 0.03], axisbg=axcolor)
+	slider_time = Slider(ax_time, 'time', 0.0, i_max, valinit=i_start,valfmt='%i')
+	#
+	# define slider update funcion
+	#
+	def update(val):
+		i = int(round(slider_time.val))
+		if y==None:
+			#
+			# update line data
+			#
+			l.set_ydata(data[i])
+		else:
+			#
+			# update 2D data
+			#			
+			while len(clist)!=0:
+				for col in clist:
+					ax.collections.remove(col)
+					clist.remove(col)
+			dummy  = ax.contourf(x,y,data[i*ny+np.arange(ny),:],np.linspace(zlim[0],zlim[-1],ncont),cmap=cmap)
+			for d in dummy.collections:
+				clist.append(d)
+		#
+		# update additional lines
+		#
+		for d,l2 in zip(data2,add_lines):
+			l2.set_ydata(d[i])
+		#
+		# update title
+		#
+		if times!=None: ti.set_text('%g'%times[i])
+		#
+		# update plot
+		#
+		plt.draw()
+	slider_time.on_changed(update)
+	#
+	# set xlog button
+	#
+	ax_xlog = plt.axes([0.5, 0.025, 0.1, 0.04])
+	button_xlog = Button(ax_xlog, 'xscale', color=axcolor, hovercolor='0.975')
+	def xlog_callback(event):
+		if ax.get_xscale() == 'log':
+			ax.set_xscale('linear')
+		else:
+			ax.set_xscale('log')
+		plt.draw()
+	button_xlog.on_clicked(xlog_callback)
+	#
+	# set ylog button
+	#
+	ax_ylog = plt.axes([0.6, 0.025, 0.1, 0.04])
+	button_ylog = Button(ax_ylog, 'yscale', color=axcolor, hovercolor='0.975')
+	def ylog_callback(event):
+		if ax.get_yscale() == 'log':
+			ax.set_yscale('linear')
+		else:
+			ax.set_yscale('log')
+		plt.draw()
+	button_ylog.on_clicked(ylog_callback)
+	#
+	# plot button
+	#
+	ax_plotbutton = plt.axes([0.8, 0.025, 0.1, 0.04])
+	button_plot = Button(ax_plotbutton, 'plot', color=axcolor, hovercolor='0.975')
+	def plotbutton_callback(event):
+		# ===================================================
+		# this part is copied from above, replacing ax=>newax
+		# and getting the snapshot index from the slider
+		# ===================================================
+		#
+		newfig=plt.figure()
+		newax    = plt.subplot(111)
+		i        = int(round(slider_time.val))
+		plt.axis([xlim[0], xlim[1], ylim[0], ylim[1]])
+		#
+		# draw labels
+		#
+		if xlabel!='': plt.xlabel(xlabel)
+		if ylabel!='': plt.ylabel(ylabel)
+		if times!=None: ti=plt.title('%g'%times[i])
+		#
+		# set scales
+		#
+		if xlog: newax.set_xscale('log')
+		if ylog: newax.set_yscale('log')
+		#
+		# plot the normal data
+		#
+		if y==None:
+			#
+			# line data
+			#
+			l, = newax.plot(x,data[i], lw=2,ls=lstyle)
+		else:
+			#
+			# 2D data
+			#
+			l  = newax.contourf(x,y,data[i*ny+np.arange(ny),:],np.linspace(zlim[0],zlim[-1],ncont),cmap=cmap)
+			clist = l.collections[:]
+		#
+		# plot additional line data
+		#
+		add_lines = []
+		for d in data2:
+			l2, = newax.plot(x,d[i], lw=2,ls=lstyle)
+			add_lines+=[l2]
+		#
+		# =========================================
+		# now set the limits as in the other figure
+		# =========================================
+		newax.set_xlim(ax.get_xlim())
+		newax.set_ylim(ax.get_ylim())
+		newax.set_xscale(ax.get_xscale())
+		newax.set_yscale(ax.get_yscale())
+		#
+		# set the original figure active again
+		#
+		j=0
+		while os.path.isfile('figure_%03i.pdf'%j): j+=1
+		fname = 'figure_%03i.pdf'%j
+		plt.savefig(fname)
+		print('saved %s'%fname)
+		plt.close(newfig)
+	button_plot.on_clicked(plotbutton_callback)
+	#
+	# GO
+	#
+	plt.show()
+	
+if __name__ == "__main__":
+	main()
